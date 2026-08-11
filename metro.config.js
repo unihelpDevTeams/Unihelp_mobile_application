e0a263007ef7d7c8b2caa8731b8588560154c6ab
Agbaje Joshua <agbajejoshua36@gmail.com>
2026-08-11 08:20:45 +0100

    fix: resolve lightningcss and pdf-lib bundling errors

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,8 @@ const { withNativewind } = require("nativewind/metro");
  
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push('mjs');
+config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
  
 module.exports = withNativewind(config);
